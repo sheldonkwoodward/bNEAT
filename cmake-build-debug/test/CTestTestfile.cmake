0ce24884b40a7efa147c3cf5bc3be6b3eb8433d7
Charles Lee Lambert <c.lambert1213@gmail.com>
2018-05-21 20:16:51 -0700

    Updated gitignore for cmake-build

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/charlie/School/Spring 2018/Artificial Intelligence/final_project/neat/test
-# Build directory: /home/charlie/School/Spring 2018/Artificial Intelligence/final_project/neat/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
