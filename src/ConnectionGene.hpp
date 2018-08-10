21bcb72a067c247ab708c968054d0ef998b13458
Charles Lee Lambert <c.lambert1213@gmail.com>
2018-08-09 23:45:11 -0700

    Refactor connectionGene to use a struct

--- src/ConnectionGene.hpp
+++ src/ConnectionGene.hpp
@@ -10,39 +10,25 @@
 #include "Node.hpp"
 
 
-class ConnectionGene {
-    Node* from;
-    Node* to;
-    float weight;
-    bool enabled;
-    int innovation;
-    unsigned int layer;
-    static unsigned int innovationCount;
+struct ConnectionGene {
+  Node *from;
+  Node *to;
+  float weight;
+  bool enabled;
+  int innovation;
+  unsigned int layer;
+  static unsigned int innovationCount;
 
-public:
-    // constructor
-    ConnectionGene(Node* from, Node* to, float weight);
-    ConnectionGene(Node* from, Node* to, float weight, unsigned int innovation);
-    ConnectionGene(Node* from, Node* to, float weight, unsigned int innovation, bool enabled);
+  // constructor
+  ConnectionGene(Node *from, Node *to, float weight);
+  ConnectionGene(Node *from, Node *to, float weight, unsigned int innovation);
+  ConnectionGene(Node *from, Node *to, float weight, unsigned int innovation,
+                 bool enabled);
 
-    // sort
-    static bool layerSort(ConnectionGene *cg1, ConnectionGene *cg2);
-    static bool innovationSort(ConnectionGene &cg1, ConnectionGene &cg2);
-    static bool innovationSortPtr(ConnectionGene* cg1, ConnectionGene* cg2);
-
-    // get set
-    Node* getFrom();
-    Node* getTo();
-    float getWeight();
-    void setWeight(float weight);
-    bool getEnabled();
-    void setEnabled(bool enabled);
-    int getInnovation();
-    void setInnovation(int innovation);
-    unsigned int getLayer();
-    void setLayer(unsigned int layer);
-    static unsigned int getInnovationCount();
+  // sort
+  static bool layerSort(ConnectionGene *cg1, ConnectionGene *cg2);
+  static bool innovationSort(ConnectionGene &cg1, ConnectionGene &cg2);
+  static bool innovationSortPtr(ConnectionGene *cg1, ConnectionGene *cg2);
 };
 
-
-#endif //NEAT_CONNECTIONGENE_HPP
+#endif // NEAT_CONNECTIONGENE_HPP
